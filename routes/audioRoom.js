826fc082d442bc57d9862e9d7e38923513804544
huangweilong <hwl5077@foxmail.com>
2017-11-15 18:27:29 +0800

    debug

--- routes/audioRoom.js
+++ routes/audioRoom.js
@@ -615,7 +615,7 @@ router.delete('/audio/blockList',
         roomId
       } = ctx.request.body
       let ownerId = ctx.decode.userId
-      console.log(`userId is ${userId}, roomId is %${roomId} ownerId is ${roomId}`)
+      console.log(`------> userId is ${userId}, roomId is %${roomId} ownerId is ${ownerId}`)
       let query = new AV.Query('AudioRoom')
       query.equalTo('objectId', roomId)
       let room = await query.first()
