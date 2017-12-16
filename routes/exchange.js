a8a2cecd20ec6d3a04c16a90853dc23a43dfa9db
huangweilong <hwl5077@foxmail.com>
2017-12-16 18:12:09 +0800

    add wxpt pay and phone verify

--- routes/exchange.js
+++ routes/exchange.js
@@ -11,8 +11,87 @@ const log4js = require('koa-log4')
 const logger = log4js.getLogger('router')
 const middle = require('../lib/middle')
 const giftMap = require('../lib/func').giftMap
+const rp = require('request-promise');
 
 router.prefix('/v1')
+
+router.post('/wxgzh',
+  async(ctx, next) => {
+    try {
+      let str = `https://api.weixin.qq.com/`
+      let data = ctx.request.body
+      if (ctx.request.body.url) {
+        str += ctx.request.body.url
+      }
+      let options = {
+        method: 'POST',
+        uri: str,
+        body: data,
+        json: true
+      }
+      let res = await rp(options)
+    } catch (err) {
+      ctx.body = {
+        status: -1,
+        data: {},
+        msg: `post wxgzh err ->${err}`
+      }
+    }
+  }
+)
+
+router.get('/wxgzh',
+  async(ctx, next) => {
+    try {
+      let str = `https://api.weixin.qq.com/`
+      if (ctx.query.url) {
+        str += ctx.query.url
+      }
+      let token = await getToken()
+      let options = {
+        uri: str,
+        qs: {
+          access_token: token
+        },
+        headers: {
+          'User-Agent': 'Request-Promise'
+        },
+        json: true
+      }
+      let res = await rp(options)
+    } catch (err) {
+      ctx.body = {
+        status: -1,
+        data: {},
+        msg: `get wxgzh err ->${err}`
+      }
+    }
+  }
+)
+
+const getToken = () => {
+  return new Promise(async(resolve, reject) => {
+    try {
+      let options = {
+        url: `https://api.weixin.qq.com/cgi-bin/token`,
+        qs: {
+          grant_type: 'client_credential',
+          appid: 'wxfa28b8b58582d1a7',
+          secret: '367fae699db82e69782715588c57a885'
+        },
+        headers: {
+          'User-Agent': 'Request-Promise'
+        },
+        json: true
+      }
+      let res = await rp(options)
+      console.log(`res -> ${JSON.stringify(res)}`)
+      resolve(res)
+    } catch (err) {
+      reject(`getToken err -> ${err}`)
+    }
+  })
+}
 
 router.get('/rate',
   jwt.verify,
@@ -82,14 +161,21 @@ const exchange = (userId, type, amount) => {
           if (_.isEmpty(ret) || ret[0].yumao_num < 1667) {
             reject(`余额不足1667羽毛,不允许提现`)
           } else {
-            let nub = util.oprate(amount, 0.6, 'mul')
-            nub = util.oprate(nub, 10, 'div')
-            sql = `update Wallet set yumao_num="${ret[0].yumao_num-amount}" where userId="${userId}"`
-            let result = await db.excute(sql)
-            if (!_.isUndefined(result)) {
-              sql = `insert into YumaoConsume values(null, "${userId}", "${orderNo}", "提现", "${amount}", "${ret[0].yumao_num-amount}", "${nub}", "${time}", "${config.rate}")`
-              result = await db.excute(sql)
-              resolve(result)
+            sql = `select * from WxUser where userId="${userId}"`
+            let wxUserInfo = await db.excute(sql)
+            if (_.isEmpty) {
+              reject(`请先授权`)
+            } else {
+              let nub = util.oprate(amount, 0.6, 'mul')
+              nub = util.oprate(nub, 10, 'div')
+              sql = `update Wallet set yumao_num="${ret[0].yumao_num-amount}" where userId="${userId}"`
+              let result = await db.excute(sql)
+              if (!_.isUndefined(result)) {
+                sql = `insert into YumaoConsume values(null, "${userId}", "${orderNo}", "提现", "${amount}", "${ret[0].yumao_num-amount}", "${nub}", "${time}", "${config.rate}")`
+                await db.excute(sql)
+                result = await pay(wxUserInfo[0].openid, nub)
+                resolve(result)
+              }
             }
           }
         } else { //兑换羽翼
@@ -116,6 +202,39 @@ const exchange = (userId, type, amount) => {
   })
 }
 
+//wx 付钱模块
+const pay = (openid, amount) => {
+  return new Promise(async(resolve, reject) => {
+    try {
+      let data = {
+        mch_appid: config.wxpt.appid,
+        mchid: ``,
+        nonce_str: ``,
+        sign: ``,
+        partner_trade_no: ``,
+        openid: openid,
+        check_name: `NO_CHECK`,
+        amount: amount,
+        desc: `提现`,
+        spbill_create_ip: ``
+      }
+      let options = {
+        method: 'POST',
+        url: `https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers`,
+        body: data,
+        json: true
+      }
+      let ret = await rp(options)
+      if(ret){
+        resolve(ret)
+      }
+      reject(`err`)
+    }catch(err){
+      reject(`pay err => ${err}`)
+    }
+  })
+}
+
 //是否够钱
 const isEnoughYumao = (userId, amount) => {
   return new Promise(async(resolve, reject) => {
@@ -132,6 +251,119 @@ const isEnoughYumao = (userId, amount) => {
     }
   })
 }
+
+// 微信授权回调接口
+router.get('/wxAuthorization',
+  async(ctx, next) => {
+    try {
+      let code = ctx.query.code
+      let time = moment().format('YYYY-MM-DD HH:MM:SS')
+      let options = {
+        url: `https://api.weixin.qq.com/sns/oauth2/access\_token`,
+        qs: {
+          appid: config.wxpt.appid,
+          secret: config.wxpt.secret,
+          code: code,
+          grant_type: authorization_code
+        },
+        headers: {
+          'User-Agent': 'Request-Promise'
+        },
+        json: true
+      }
+      let result = await rp(options)
+      options = {
+        url: `https://api.weixin.qq.com/sns/userinfo`,
+        qs: {
+          access_token: result.access_token,
+          openid: result.openid,
+          lang: zh_CN
+        },
+        headers: {
+          'User-Agent': 'Request-Promise'
+        },
+        json: true
+      }
+      /* ret form
+      {   "openid":" OPENID",
+          " nickname": NICKNAME,
+          "sex":"1",
+          "province":"PROVINCE"
+          "city":"CITY",
+          "country":"COUNTRY",
+          "headimgurl":    "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46",
+          "privilege":[ "PRIVILEGE1" "PRIVILEGE2"     ],
+          "unionid": "o6\_bmasdasdsad6\_2sgVt7hMZOPfL"
+      }
+      */
+      let ret = await rp(options)
+      let query = new AV.Query('_User')
+      query.equalTo('wxUid', ret.unionid)
+      let uesr = await query.first()
+      if (_.isUndefined(user)) {
+        return ctx.body = {
+          status: 403,
+          data: {},
+          msg: `请先在App内绑定微信`
+        }
+      } else {
+        let sql = `insert into WxUser values(null, ${user.get('objectId')}, ${ret.unionid}, ${result.openid}, ${user.get('mobilePhoneNumber')}, ${time}, ${time})`
+        let dbret = await db.excute(sql)
+        console.log(`dbret => ${JSON.stringify(dbret)}`)
+        if (_.isEmpty(dbret)) {
+          return ctx.body = {
+            status: -1,
+            data: {},
+            msg: `data err`
+          }
+        } else {
+          ctx.body = {
+            status: 200,
+            data: ret,
+            msg: `success`
+          }
+        }
+      }
+    } catch (err) {
+      ctx.body = {
+        status: -1,
+        data: {},
+        msg: `wxAuthorization err => ${err}`
+      }
+    }
+  }
+)
+
+//验证手机号
+router.post('/verifyNub',
+  async(ctx, next) => {
+    try {
+      let phoneNub = ctx.request.body.phoneNub
+      let query = new AV.Query('_User')
+      query.equalTo('mobilePhoneNumber', phoneNub)
+      let user = await query.first()
+      if (_.isUndefined(user)) {
+        return ctx.body = {
+          status: -1,
+          data: {},
+          msg: `未注册`
+        }
+      } else {
+        ctx.body = {
+          status: 200,
+          data: util.getUserInfo(user),
+          msg: `success`
+        }
+      }
+    } catch (err) {
+      ctx.body = {
+        status: -1,
+        data: {},
+        msg: `verify phone number err -> ${err}`
+      }
+    }
+  }
+)
 
 //获取兑换或提现订单记录
 router.get('/exchange',
