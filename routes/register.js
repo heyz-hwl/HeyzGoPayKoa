a8bdb34daa73b5edc9839129f0b9bc13b2f29270
huangweilong <hwl5077@foxmail.com>
2017-12-25 16:25:24 +0800

    add try catch

--- routes/register.js
+++ routes/register.js
@@ -9,55 +9,64 @@ const logger = log4js.getLogger('router')
 
 router.prefix('/v1')
 
-router.post('/register', async(ctx, next) => {
-  let {
-    phone,
-    wechat,
-    corpsName,
-    type
-  } = ctx.request.body
-
-  if (!phone || !corpsName || !wechat || !type) {
-    return ctx.body = {
-      status: 403,
-      data: {},
-      msg: `params missing`
+router.post('/register', 
+  async(ctx, next) => {
+    try {
+      let {
+        phone,
+        wechat,
+        corpsName,
+        type
+      } = ctx.request.body
+    
+      if (!phone || !corpsName || !wechat || !type) {
+        return ctx.body = {
+          status: 403,
+          data: {},
+          msg: `params missing`
+        }
+      }
+    
+      if (!(/^1[34578]\d{9}$/.test(phone))) {
+        return ctx.body = {
+          status: 403,
+          data: {},
+          msg: `wrong phone number`
+        }
+      }
+    
+      let query = new AV.Query(`_User`)
+      query.equalTo(`mobilePhoneNumber`, phone)
+      let user = await query.first()
+      if(!user){
+        return ctx.body = {
+          status: -1,
+          data: {},
+          msg: `未注册用户`
+        }
+      }
+    
+      let register = AV.Object.new(`CompeteRegister`)
+      register.set('phone', phone)
+      register.set('corpsName', corpsName)
+      register.set('type', Number(type))
+      register.set('wechat', wechat)
+      let ret = await register.save()
+      console.log(`ret is ${JSON.stringify(ret)}`)
+      if (ret) {
+        ctx.body = {
+          status: 200,
+          data: ret,
+          msg: `success`
+        }
+      }
+    } catch(err){
+      ctx.body = {
+        status: -1,
+        data: {},
+        msg: `post register err ->${err}`
+      }
     }
-  }
-
-  if (!(/^1[34578]\d{9}$/.test(phone))) {
-    return ctx.body = {
-      status: 403,
-      data: {},
-      msg: `wrong phone number`
-    }
-  }
-
-  let query = new AV.Query(`_User`)
-  query.equalTo(`mobilePhoneNumber`, phone)
-  let user = await query.first()
-  if(!user){
-    return ctx.body = {
-      status: -1,
-      data: {},
-      msg: `未注册用户`
-    }
-  }
-
-  let register = AV.Object.new(`CompeteRegister`)
-  register.set('phone', phone)
-  register.set('corpsName', corpsName)
-  register.set('type', type)
-  register.set('wechat', wechat)
-  let ret = await register.save()
-  console.log(`ret is ${JSON.stringify(ret)}`)
-  if (ret) {
-    ctx.body = {
-      status: 200,
-      data: ret,
-      msg: `success`
-    }
-  }
 })
 
 router.get('/register', async(ctx, next) => {
