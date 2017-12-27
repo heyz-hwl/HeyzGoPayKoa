3300e6ae422aee3bc550169123f57cf5d39c6d1f
huangweilong <hwl5077@foxmail.com>
2017-12-27 16:37:07 +0800

    Number

--- routes/user.js
+++ routes/user.js
@@ -326,7 +326,7 @@ router.put('/user',
         }if(height){
           user.set(`height`, Number(height))
         }if(birthday){
-          user.set(`birthday`, new Date(birthday))
+          user.set(`birthday`, new Date(Number(birthday)))
         }
         await user.save()
         ctx.body = {
