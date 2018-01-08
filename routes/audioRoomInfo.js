cca22405cd251a4a793a1bff3fc5c6cdd42f68cf
huangweilong <hwl5077@foxmail.com>
2018-01-08 18:29:08 +0800

    add deal with roomInfo data and add API quit the room

--- routes/audioRoomInfo.js
+++ routes/audioRoomInfo.js
@@ -3,7 +3,6 @@ const router = require('koa-router')()
 const jwt = require('../lib/jwt')
 const _ = require('lodash')
 const moment = require('moment')
-const db = require('../lib/db')
 const socket = require('../lib/socket')
 const log4js = require('koa-log4')
 const logger = log4js.getLogger('router')
@@ -13,14 +12,13 @@ router.prefix('/v1')
 
 //创建房间
 router.post('/room',
-  // jwt.verify,
+  jwt.verify,
   async(ctx, next) => {
     try {
       let {
-        title,
-        userId
+        title
       } = ctx.request.body
-      let owner = ctx.request.body.userId
+      let owner = ctx.decode.userId
       console.log(`owner is ${owner}`)
       let room = new Room()
       let ret = await room.createRoom(title, owner)
@@ -43,7 +41,7 @@ router.post('/room',
 
 //获取房间信息
 router.get('/room',
-  jwt.verify,
+  // jwt.verify,
   async(ctx, next) => {
     try {
       let roomId = ctx.query.roomId
@@ -106,7 +104,7 @@ router.post('/room/user',
         userId,
         position
       } = ctx.request.body
-      let room = await new Room()
+      let room = await new Room(roomId)
       let ret = await room.addMember(userId, position)
       ctx.body = {
         status: 200,
@@ -122,6 +120,50 @@ router.post('/room/user',
     }
   }
 )
+
+//用户主动退出房间时,不用传参数
+//房主或者副房主踢人时传操作者的 userId
+router.delete('/room/user',
+  jwt.verify,
+  async(ctx, next) => {
+    try {
+      let userId = ctx.request.body.userId ? ctx.request.body.userId : ctx.decode.userId
+      let {
+        operatorId,
+        roomId
+      } = ctx.request.body
+      let query = new AV.Query('AudioRoomMember')
+      let user = AV.Object.createWithoutData('_User', operatorId)
+      query.equalTo('user', user)
+      query.equalTo('position', '-1')
+      let ret = await query.first()
+      //不是房主,并且不是副房主
+      if (room.owner.get('objectId') !== operatorId && ret) {
+        ctx.body = {
+          status: 1003,
+          data: {},
+          msg: `没有权利`
+        }
+      } else {
+        //用户直接退房间 或者房主踢人 或者副房主踢人
+        let room = await new Room(roomId)
+        let ret = await room.deleteUser(userId)
+        ctx.body = {
+          status: 200,
+          data: ret,
+          msg: `success`
+        }
+      }
+    } catch (err) {
+      ctx.body = {
+        status: -1,
+        data: {},
+        msg: `delete user err ->${err}`
+      }
+    }
+  }
+)
+
 //加锁或者解锁
 //type == 1 加锁 type == 0 解锁
 router.get('/room/lock',
